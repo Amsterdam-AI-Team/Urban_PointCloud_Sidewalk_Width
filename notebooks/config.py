4dc10a07216ada5c0957ba864490bc08ce0667fe
ShaylaJan <shayla.jansen@amsterdam.nl>
2023-08-17 07:04:14 +0000

    feature: add config file setup to nb6

--- notebooks/config.py
+++ notebooks/config.py
@@ -1,11 +1,21 @@
-# Paths
+# Input paths notebook 5
 pc_data_folder = '../datasets/pointclouds/'
 out_folder = '../datasets/output/'
 obstacle_file = f'{out_folder}sidewalks_with_obstacles.gpkg'
+
+# Intermediate output path (in case of errors, notebook 5)
+tmp_file = f'{out_folder}sw_seg_tmp.pkl'
+
+# Output path notebook 5 / input path notebook 6
 segments_file = f'{out_folder}sidewalk_segments.gpkg'
 
-# Save intermediate output in case of errors
-tmp_file = f'{out_folder}sw_seg_tmp.pkl'
+# BGT path
+bgt_road_file = '../datasets/bgt/bgt_voetpad.gpkg' 
+
+# Final output paths 
+output_file = f'{out_folder}final_output_segments_all.gpkg'
+output_image = f'{out_folder}final_output_image_all.png'
+output_image_no = f'{out_folder}final_output_image_no_all.png'
 
 # Set Coordinate Reference System
 CRS = 'epsg:28992' 
@@ -30,3 +40,23 @@ width_resolution = 1
 
 # Precision (in decimals) for min and avg width computation
 width_precision = 1
+
+# Boundary for filtering out (in meters)
+min_path_width = 0.4 
+
+# Boundaries between the final colors green/orange/red (in meters)
+width_lower = 0.9  
+width_upper = 1.8
+width_top = 2.9 
+
+# Maximum distance between intended start point and start node (in meters)
+max_dist = 3 
+
+# Maximum length of linestring (in meters), otherwise cut
+max_ls_length = 10
+
+# Minimum length for short-ends (in meters), otherwise removed (in full width calculation)
+min_se_length_fw = 10
+
+# Minimum interior size to remain in BGT data
+min_interior_size = 10
--- notebooks/config_azure.py
+++ notebooks/config_azure.py
@@ -1,14 +1,24 @@
-# Paths
+# Input paths notebook 5
 pc_data_folder = "/home/azureuser/cloudfiles/code/blobfuse/ovl/pointcloud/Unlabeled/Amsterdam/Cyclomedia_overview/"
 out_folder = "/home/azureuser/cloudfiles/code/blobfuse/sidewalk/"
-obstacle_file = f'{out_folder}sidewalks_with_obstacles.gpkg'  # sidewalks_with_obstacles_all.gpkg
-segments_file = f'{out_folder}sidewalk_segments.gpkg'
+obstacle_file = f'{out_folder}sidewalks_with_obstacles_all.gpkg'  # sidewalks_with_obstacles.gpkg or sidewalks_with_obstacles_all.gpkg
 
-# Save intermediate output in case of errors
+# Intermediate output path (in case of errors, notebook 5)
 tmp_file = f'{out_folder}sw_seg_tmp.pkl'
 
+# Output path notebook 5 / input path notebook 6
+segments_file = f'{out_folder}processed_data/output/sidewalk_segments.gpkg'  # sidewalk_segments_small.gpkg or sidewalk_segments.gpkg
+
+# BGT path
+bgt_road_file = f'{out_folder}processed_data/bgt/bgt_voetpad_small.gpkg'  # bgt_voetpad.gpkg
+
+# Final output paths
+output_file = f'{out_folder}processed_data/output/final_output_segments_all.gpkg'
+output_image = f'{out_folder}processed_data/output/final_output_image_all.png'
+output_image_no = f'{out_folder}processed_data/output/final_output_image_no_all.png'
+
 # Set Coordinate Reference System
-CRS = 'epsg:28992' 
+CRS = 'epsg:28992'
 
 # Whether to merge sidewalks before segmentation and width computation
 merge_sidewalks = True
@@ -30,3 +40,23 @@ width_resolution = 1
 
 # Precision (in decimals) for min and avg width computation
 width_precision = 1
+
+# Boundary for filtering out (in meters)
+min_path_width = 0.4 
+
+# Boundaries between the final colors green/orange/red (in meters)
+width_lower = 0.9  
+width_upper = 1.8
+width_top = 2.9 
+
+# Maximum distance between intended start point and start node (in meters)
+max_dist = 3 
+
+# Maximum length of linestring (in meters), otherwise cut
+max_ls_length = 10
+
+# Minimum length for short-ends (in meters), otherwise removed (in full width calculation)
+min_se_length_fw = 10
+
+# Minimum interior size to remain in BGT data
+min_interior_size = 10
